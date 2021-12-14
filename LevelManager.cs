138f3a363172159e0dbd2a3c4ed2579ae16f8316
Hunt1C <94756065+Hunt1C@users.noreply.github.com>
2021-12-14 08:37:29 -0500

    Add files via upload

---
+++ LevelManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    //Loads lvl after x number of seconds given in the inspector
+    public float autoLoadNextLevelAfter;
+
+   
+
+    void Start()
+    {
+        
+        if (autoLoadNextLevelAfter == 0)
+        {
+            Debug.Log("Level auto load disabled");
+        } else
+        {
+            Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+        }
+        
+    }
+
+    //call this to load a new scene
+    public void LoadLevel(string name){
+		Debug.Log ("New Level load: " + name);
+		SceneManager.LoadScene (name);
+	}
+
+    //call this to quit a game
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+    //call this to lead the next level avalible in the Build Settings
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene("Start Menu");
+        SceneManager.LoadScene("Play Menu");
+    }
+}
